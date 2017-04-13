5dc479c31d8d0b8fd32e7109bf9b36fac5c9fd71
Clayton Hadaway <fluffyquake@gmail.com>
2017-04-13 01:35:27 +0000

    Fix port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const latestSearches = require('./src/latestSearches');
 const app = express();
 
 app.use(express.static('public'));
-app.set('port', process.argv.PORT || 8080);
+app.set('port', process.argv.PORT || 5000);
 
 app.get('/search', (request, response) => response.json({"error": "no query supplied"}));
 app.get('/search/:search?', imageSearch);
